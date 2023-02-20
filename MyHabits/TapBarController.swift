2be945ae3be26cd844f7b229b095cd50f9a734e0
donskikhmaksim <donskikh.maksim@gmail.com>
2023-02-20 17:04:06 +0300

    TabBarController class has been added

---
+++ MyHabits/TapBarController.swift
@@ -0,0 +1,19 @@
+//
+//  TapBarController.swift
+//  MyHabits
+//
+//  Created by Максим Донских on 20.02.2023.
+//
+
+import UIKit
+
+final class TabBarController: UITabBarController {
+    init(viewControllers: [ViewController]) {
+        super.init(nibName: nil, bundle: nil)
+        self.viewControllers = viewControllers
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
